fe42392a00835667ec51d814df31bec323c98e3c
doodzik <frederik.dudzik@gmail.com>
2015-06-02 18:31:49 +0200

    init

---
+++ bin/startup.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  # Do something under Mac OS X platform
+  source $(brew --prefix nvm)/nvm.sh
+fi
+
+# npm run webpack -- --progress --colors --watch &
+./node_modules/.bin/webpack --progress --colors --watch &
+nvm run server.js
